2700c6568ab56ce731186ad9cb87a969271cff90
Iness <ines.sebastian.santamaria@gmail.com>
2026-02-02 15:53:49 +0100

    new buttons, prettier code, new function in main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,40 @@
+	function ocultarOpciones() {
+
+		if (window.location.pathname.includes('sign-in.html')) {
+			const changeuser = document.getElementById('changeuser-btn');
+			if (changeuser) {
+				changeuser.style.display = 'none';
+			}
+
+		}
+
+		if (window.location.pathname.includes('index.html')) {
+			const gobackBtn = document.getElementById('goback-btn');
+			if (gobackBtn) {
+				gobackBtn.style.display = 'none';
+			}
+			const loginbtn = document.getElementById('login-btn');
+			if(localStorage.getItem('isLoggedIn') === 'true'){
+				if(loginbtn){
+					loginbtn.style.display = 'none';
+				}
+			}
+		}
+
+		if (window.location.pathname.includes('carroCompra.html')) {
+			const cartbtn = document.getElementById('cart-options');
+			if(cartbtn){
+				cartbtn.style.display = 'none';
+			}
+		}
+	}
+	
 	$(function(){   
 		$("#footer-placeholder").load("footer.html");
+		ocultarOpciones();
 	});
     $(function(){   
 		$("#header-global").load("header.html");
+		ocultarOpciones();
 	});
+
