adb95d23f3daa665892bc561261dd5468080f8db
Iness <ines.sebastian.santamaria@gmail.com>
2026-02-03 23:25:42 +0100

    prettier code, log out button, hide options to Admin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
-	function ocultarOpciones() {
-
-		if (window.location.pathname.includes('catalogoProductos.html')) {
-			const productos= document.getElementById('productos');
-			if (productos) {
-				productos.style.display = 'none';
+	function hideOptionsAdmin(cartbtn,headerbtnToCatalogPlants){
+		if(localStorage.getItem('isAdmin') === 'true'){
+			if(cartbtn){
+				cartbtn.style.display = 'none';
 			}
+			if (headerbtnToCatalogPlants) {
+				headerbtnToCatalogPlants.style.display = 'none';
+			}
+			const botonForSeeingPlants= document.getElementById('Plantscatalog')
+			if (botonForSeeingPlants) {
+				botonForSeeingPlants.style.display = 'none';
+			}
+			document.querySelectorAll('.addToCartbtn').forEach(btn => { //esto es para eliminar flechas si solo hay una imagen
+				btn.style.display = 'none';
+			});
+		} else{
+			const adminbtn = document.getElementById('admin-btn');
+			adminbtn.style.display = 'none';
 		}
-
+	}
+	function hideOptionsMainPage(){
 		if (window.location.pathname.includes('index.html')) {
 			const gobackBtn = document.getElementById('goback-btn');
 			if (gobackBtn) {
@@ -27,6 +39,20 @@
 				plantitas.style.display = 'none';
 			}
 		}
+	}
+	function hideOptions() {
+		const cartbtn = document.getElementById('cart-options');
+		const headerbtnToCatalogPlants= document.getElementById('plantitas');
+		hideOptionsAdmin(cartbtn,headerbtnToCatalogPlants);
+		hideOptionsMainPage();
+
+		if (window.location.pathname.includes('catalogoProductos.html')) {
+			const productos= document.getElementById('productos');
+			if (productos) {
+				productos.style.display = 'none';
+			}
+		}
+
 
 		if (window.location.pathname.includes('sign-in.html')) {
 			const changeuser = document.getElementById('changeuser-btn');
@@ -37,16 +63,14 @@
 		
 
 		if (window.location.pathname.includes('carroCompra.html')) {
-			const cartbtn = document.getElementById('cart-options');
 			if(cartbtn){
 				cartbtn.style.display = 'none';
 			}
 		}
 
 		if (window.location.pathname.includes('catalogoPlantas.html')) {
-			const plantitas= document.getElementById('plantitas');
-			if (plantitas) {
-				plantitas.style.display = 'none';
+			if (headerbtnToCatalogPlants) {
+				headerbtnToCatalogPlants.style.display = 'none';
 			}
 		}
 
@@ -54,10 +78,10 @@
 	
 	$(function(){   
 		$("#footer-global").load("footer.html");
-		ocultarOpciones();
+		hideOptions();
 	});
     $(function(){   
 		$("#header-global").load("header.html");
-		ocultarOpciones();
+		hideOptions();
 	});
 
