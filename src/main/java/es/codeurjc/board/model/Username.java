8178cfd05df333e569acfed26871d1ed4594b8f6
SGL23 <s.garcial.2024@alumnos.urjc.es>
2026-02-24 10:42:59 +0100

    table of users and controller

---
+++ src/main/java/es/codeurjc/board/model/Username.java
@@ -0,0 +1,81 @@
+package es.codeurjc.board.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+public class Username {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+    private String description;
+    private String profileImage;
+
+    @OneToMany (mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reviews> reviews =new ArrayList<>();
+
+    public Username() {}
+
+    public Username(String email, String password, String username, String description) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+}
