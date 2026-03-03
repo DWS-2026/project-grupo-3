022fcaf10547a441a08f5a30eb23c651090a89cd
SGL23 <s.garcial.2024@alumnos.urjc.es>
2026-03-03 20:08:22 +0100

    added @RequestParam to required items and functions

--- src/main/java/es/codeurjc/board/controller/UserController.java
+++ src/main/java/es/codeurjc/board/controller/UserController.java
@@ -1,16 +1,24 @@
 package es.codeurjc.board.controller;
+import es.codeurjc.board.model.Username;
 import es.codeurjc.board.modelAttributes.ButtonsHeader;
+import es.codeurjc.board.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
     @Autowired
     private ButtonsHeader btnsHeader;
 
+    private boolean passwordsDontMatch= false;
+    private boolean existsUsername = false;
+
+    @Autowired
+    private UserService userService;
     @GetMapping("/User/user")
     public String user(Model model) {
         btnsHeader.hideBtnHeader(model,"profile");
@@ -33,7 +41,28 @@ public class UserController {
     }
 
     @PostMapping("/User/register")
-    public String register(){return "redirect:index";}
+    public String register(Model model,@RequestParam String password, @RequestParam String email, @RequestParam String username, @RequestParam String repeatpassword){
+        model.addAttribute("passwords",passwordsDontMatch);
+        Username user = new Username();
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setUsername(username);
+       //user.setRoles(); //Hay que añadirle un lista de string
+
+        if(!password.equals(repeatpassword)){ //mirar si las contraseñas coinciden
+            passwordsDontMatch = true;
+            return "redirect:/User/register";
+        }
+
+        if(!existsUsername){
+
+        }
+
+        userService.saveUser(user);
+        return "redirect:index";}
+
+    //@PostMapping --> aquí tengo que el postmapping para eliminar un usuario desde el panel del admin
+
 
 
 }
