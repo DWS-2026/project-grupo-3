9ea82e012c45506cdb5a824961383ee8f5820872
SGL23 <s.garcial.2024@alumnos.urjc.es>
2026-03-03 20:10:05 +0100

    created UserService for repository

---
+++ src/main/java/es/codeurjc/board/service/UserService.java
@@ -0,0 +1,22 @@
+package es.codeurjc.board.service;
+
+
+import es.codeurjc.board.model.Username;
+import es.codeurjc.board.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser (Username user){
+        userRepository.save(user);
+    }
+
+    public boolean usernameExist(String username) {
+        return userRepository.existsUsernamesByUsername(username);
+    }
+}
