6990d2d10ae38ad066e1fb1721b93086e269eca6
Iness <ines.sebastian.santamaria@gmail.com>
2026-04-21 14:52:24 +0200

    now we can login via api

--- src/main/java/es/codeurjc/board/security/SecurityConfig.java
+++ src/main/java/es/codeurjc/board/security/SecurityConfig.java
@@ -4,17 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.codeurjc.board.security.jwt.*;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class SecurityConfig {
+    @Autowired
+  	private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+    	
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     public RepositoryUserDetailsService userDetailService;
@@ -31,34 +42,25 @@ public class WebSecurityConfig {
 
         return authProvider;
     }
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+	
     @Bean
     @Order(1)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
 
-        http.authenticationProvider(authenticationProvider());
-
+        http.authenticationProvider(authenticationProvider()); 
+        
         http
-                .securityMatcher("/api/**");
-        //.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+			.securityMatcher("/api/v1/**")
+			.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+		
 
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        // PRIVATE ENDPOINTS
-                        // Images
-                        //.requestMatchers(HttpMethod.PUT, "/api/images/*/media").hasRole("USER")
-                        //.requestMatchers(HttpMethod.DELETE, "/api/books/*/images/*").hasRole("USER")
-                        // Books
-                        //.requestMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER")
-                        //.requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER")
-                        //.requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
-                        // Shops
-//                        .requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/api/shops/**").hasRole("ADMIN")
-//                        // PUBLIC ENDPOINTS
-                        .anyRequest().permitAll());
-
+        http.authorizeHttpRequests(authorize -> authorize.
+            anyRequest().permitAll());
+		
         // Disable Form login Authentication
         http.formLogin(formLogin -> formLogin.disable());
 
@@ -71,10 +73,10 @@ public class WebSecurityConfig {
         // Stateless session
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // Add JWT Token filter
-        //http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+		// Add JWT Token filter
+		http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
+		return http.build();
     }
 
 
