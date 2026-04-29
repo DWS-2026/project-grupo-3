303bc3f11936e20ed35d2c5176a998e9b3c1d431
Iness <ines.sebastian.santamaria@gmail.com>
2026-04-30 00:06:31 +0200

    we got api documentation

--- src/main/java/es/codeurjc/board/security/SecurityConfig.java
+++ src/main/java/es/codeurjc/board/security/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(authorize -> authorize
                         // PUBLIC PAGES
-                        .requestMatchers("/", "/assets/css/**", "/assets/images/**", "/error", "/403", "/login", "/check").permitAll()
+                        .requestMatchers("/", "/assets/css/**", "/assets/images/**", "/error", "/403", "/login", "/check", "/images/*").permitAll()
+                        .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
                         .requestMatchers("/User/register").permitAll()
                         .requestMatchers("/images/*").permitAll()
                         .requestMatchers("/Plants/catalogPlants").permitAll()
