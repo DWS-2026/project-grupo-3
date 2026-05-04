d2ad264133fe2806b655067aa3545b608da1280b
SGL23 <s.garcial.2024@alumnos.urjc.es>
2026-05-04 11:51:38 +0200

    add variables to UserBasicDTO

--- src/main/java/es/codeurjc/board/rest/dto/UserBasicDTO.java
+++ src/main/java/es/codeurjc/board/rest/dto/UserBasicDTO.java
@@ -1,5 +1,5 @@
 package es.codeurjc.board.rest.dto;
 
-public record UserBasicDTO (String username, String description){
+public record UserBasicDTO (String username, String description, String email, ImageDTO images){
     
 }
