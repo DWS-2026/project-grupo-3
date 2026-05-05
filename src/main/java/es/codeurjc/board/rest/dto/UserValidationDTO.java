790b9e14164b7245db2a52bdb1d0f176d4acc641
SGL23 <s.garcial.2024@alumnos.urjc.es>
2026-05-05 09:59:01 +0200

    add UserValidationDTO to dto

---
+++ src/main/java/es/codeurjc/board/rest/dto/UserValidationDTO.java
@@ -0,0 +1,20 @@
+package es.codeurjc.board.rest.dto;
+import jakarta.validation.constraints.*;
+
+public record UserValidationDTO ( @NotBlank(message = "El username es obligatorio")String username,
+
+    String description,
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email no es válido")
+    String email,
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "Mínimo 8 caracteres")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._-]).*$",
+        message = "Debe tener mayúsculas, minúsculas, números y un carácter especial"
+    )
+    String password
+
+) {}
